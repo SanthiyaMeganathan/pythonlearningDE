370da1e74647d1420dd1238af509e33213e39f6d
SanthiyaMeganathan <meganathankmsanthiya@gmail.com>
2026-03-13 15:41:36 +0530

    loop

---
+++ loop.py
@@ -0,0 +1,40 @@
+#no condition and predict number of times it run
+
+names=["don","din","jhon"]
+
+for name in names:
+    print(name)
+    print("welcome to the party")
+
+#while
+correct_pin='12345'
+entered_pin=''
+#we dont know how many times loops and we have condition
+
+while entered_pin != correct_pin:
+    entered_pin =input("enter the pin:   ")
+
+print("you have entered the correct pin")   
+
+#break
+#we use it to stop loop once the condition satisfies
+
+for i in range(10):
+    if i==5:
+        break 
+    print(i)
+
+#continue->to skip
+
+
+nums=[10,12,56,89,-1]
+
+for n in nums:
+    if n<0:
+        continue
+    print(n)
+
+
+    
+
+
